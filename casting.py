bd7881f11830ed1b96ea96c6b8975421a3368044
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-08 21:12:15 -0500

    Actualizacion de casting.py

--- casting.py
+++ casting.py
@@ -1,6 +1,6 @@
 print("ingresa el primer punto")
 num1 = int(input())
 print("ingresa el segundo punto")
-num2 = int(input())
+num2 = float(input())
 resultado = num1 + num2 
 print("El resultado es:" + str(resultado))
