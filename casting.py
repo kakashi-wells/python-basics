274524a2f934027a0fe5afcd577e5d170657ec9f
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-08 22:51:29 -0500

    Creacion de Carpeta funciones.py

--- casting.py
+++ casting.py
@@ -3,6 +3,6 @@ num1 = int(input())
 print("ingresa el segundo punto")
 num2 = int(input())
 print("ingresa el tercer punto")
-num3 = float(input())
+num3 = int(input())
 resultado = num1 + num2 + num3
 print("El resultado es:" + str(resultado))
