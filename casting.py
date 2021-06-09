3dc95a88eedada0aaf671022e4ef7b5cda2075be
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-08 21:18:41 -0500

    Actualizacion de casting.py agrenando * en vez de +

--- casting.py
+++ casting.py
@@ -1,6 +1,6 @@
 print("ingresa el primer punto")
 num1 = int(input())
 print("ingresa el segundo punto")
-num2 = float(input())
+num2 = int(input())
 resultado = num1 + num2 
-print("El resultado es:" + str(resultado))
+print("El resultado es:" * resultado)
