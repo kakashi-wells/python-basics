8a887e6040ef0f46f86f73d9a4ade7ee34e89197
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-08 22:15:57 -0500

    Agregar 3 puntos para salir numero de veces repetido

--- casting.py
+++ casting.py
@@ -2,5 +2,7 @@ print("ingresa el primer punto")
 num1 = int(input())
 print("ingresa el segundo punto")
 num2 = int(input())
-resultado = num1 + num2 
+print("ingresa el tercer punto")
+num3 = int(input())
+resultado = num1 + num2 + num3
 print("El resultado es:" * resultado)
