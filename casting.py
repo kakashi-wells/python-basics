92985e58766e1f3dca42e868175ac4b99160f9f3
kakashi-wells <arthurbedoya99@gmail.com>
2021-06-08 22:17:45 -0500

    Agregar suma con numeros decimales

--- casting.py
+++ casting.py
@@ -3,6 +3,6 @@ num1 = int(input())
 print("ingresa el segundo punto")
 num2 = int(input())
 print("ingresa el tercer punto")
-num3 = int(input())
+num3 = float(input())
 resultado = num1 + num2 + num3
-print("El resultado es:" * resultado)
+print("El resultado es:" + str(resultado))
